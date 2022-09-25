5cb0cc9d033fd93905747d434e094f0f691357c1
Ian H. Kessler <ianholm9@gmail.com>
2022-09-24 18:44:40 -0600

    Created init.

--- IBL.py
+++ IBL.py
@@ -3,5 +3,5 @@ import EthanClass
 
 
 class IBL (IanClass, EthanClass):
-    def __init__(self):
-        pass
+    def __init__(self, file, features, name, classLoc, replaceValue = None):
+        IanClass.__init__(self, file, features, name, classLoc, replaceValue = None)
--- IanClass.py
+++ IanClass.py
@@ -6,5 +6,28 @@ from ConfusionMatrix import ConfusionMatrix
 import numpy as np
 
 class IanClass:
-    def __init__(self):
-        pass
+    def __init__(self, file, features, name, classLoc, replaceValue = None):
+        df = pd.read_csv(os.getcwd() + r'\Raw Data' + '\\' + file)
+        self.df = df  # dataframe
+        self.features = features
+        self.name = name
+        self.addColumnNames(classLoc)  # add column names to correct spot
+        self.classes = list(set(self.df['Class']))
+        if replaceValue: self.findMissing(replaceValue)  # replace missing values
+        self.df.to_csv(
+            os.getcwd() + '\\' + str(self) + '\\' + "{}_w_colnames.csv".format(str(self)))  # create csv of file
+        self.seed = random.random()
+
+    def __str__(self):
+        return self.name
+
+    def addColumnNames(self, classLoc):
+        if (classLoc == 'beginning'):  # if the class column is at the beginning
+            self.df.columns = ['Class'] + self.features
+            # shift the class column to the last column
+            last_column = self.df.pop('Class')
+            self.df.insert(len(self.df.columns), 'Class', last_column)
+        elif (classLoc == 'end'):  # if the class column is at the end -> continue as normal
+            self.df.columns = self.features + ['Class']
+        else:
+            print('Not sure where to place Class column')
