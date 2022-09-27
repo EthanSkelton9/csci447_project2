0436c72b79476c67b16288f47e162c6c5ee55340
Ethan10Real <ethan@10Real.com>
2022-09-26 19:44:21 -0600

    adding structure to talk about on tuesday

--- Glass.py
+++ Glass.py
@@ -2,4 +2,19 @@ import IBL
 
 class Glass (IBL):
     def __init__(self):
-        pass
+        #list of feature names(excluding class)
+        features = [ #Class at end
+            "Id number: 1 to 214",
+            "RI: refractive index",
+            "Na: Sodium",
+            "Mg: Magnesium",
+            "Al: Aluminum",
+            "Si: Silicon",
+            "K: Potassium",
+            "Ca: Calcium",
+            "Ba: Barium",
+            "Fe: Iron"
+            ]
+
+        #initiate glass test set
+        super().__init__(file='glass.csv', features = features, name = 'Glass', classLoc = 'end')
--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 from k_nearest import K_Nearest
 from SoyBean import SoyBean
 from Abalone import Abalone
+from Glass import Glass
 import os
 
 '''
     Driver for k Nearest Neighbor    
 '''
 def main_Ethan():
-    ds = K_Nearest("hello")
-    ds.TrainFile()  #train file and return with new hyperparameters to input
 
+    #hyperparameters:
+
+    datasets = []
+    datasets.append(Glass().Learning())
+
+    for ds in datasets:
+        k = K_Nearest()
+        train = k.TrainFile(ds)
+        test = k.Test_file(ds, train)
+        #change hyperparameters for next dataset
+    
 def main_Ian():
     D = Abalone()
 
